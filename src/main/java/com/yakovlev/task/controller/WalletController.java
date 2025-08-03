9205f68cd54f86f3524c2922fa6e85c11d43c80d
Alexey Yakovlev <alexio98@yandex.ru>
2025-08-03 13:54:18 +0300

    -create WalletController

--- src/main/java/com/yakovlev/task/controller/WalletController.java
+++ src/main/java/com/yakovlev/task/controller/WalletController.java
@@ -1,8 +1,13 @@
 package com.yakovlev.task.controller;
 
+import com.yakovlev.task.model.OperationType;
+import com.yakovlev.task.model.WalletDTO;
 import com.yakovlev.task.service.WalletService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.yakovlev.task.util.exception.WalletNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 /**
  * Created by alexi on 03.08.2025
@@ -14,5 +19,26 @@ public class WalletController {
 
     public WalletController(WalletService walletService) {
         this.walletService = walletService;
+    }
+
+    @GetMapping("/wallets/{WALLET_UUID}")
+    @ResponseStatus(HttpStatus.OK)
+    public WalletDTO getWalletById(@PathVariable UUID WALLET_UUID) throws WalletNotFoundException {
+        return walletService.getById(WALLET_UUID);
+    }
+
+    @PostMapping("/wallet/save")
+    @ResponseStatus(HttpStatus.CREATED)
+    public WalletDTO createWallet(@RequestBody WalletDTO walletDTO) {
+        return walletService.save(walletDTO);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public WalletDTO deposit(@RequestBody WalletDTO walletDTO) throws WalletNotFoundException {
+        if (walletDTO.getOperationType().equals(OperationType.DEPOSIT))
+            return walletService.deposit(walletDTO);
+        else
+            return walletService.withdraw(walletDTO);
     }
 }
--- src/main/java/com/yakovlev/task/service/impl/WalletServiceImpl.java
+++ src/main/java/com/yakovlev/task/service/impl/WalletServiceImpl.java
@@ -6,12 +6,16 @@ import com.yakovlev.task.repository.WalletRepository;
 import com.yakovlev.task.service.WalletService;
 import com.yakovlev.task.util.WalletMapper;
 import com.yakovlev.task.util.exception.WalletNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
 /**
  * Created by alexi on 03.08.2025
  */
+@Service
+@Transactional(readOnly = true)
 public class WalletServiceImpl implements WalletService {
 
     private final WalletRepository walletRepository;
@@ -33,12 +37,14 @@ public class WalletServiceImpl implements WalletService {
     }
 
     @Override
+    @Transactional
     public WalletDTO save(WalletDTO walletDTO) {
         Wallet walletToSave = WalletMapper.toWallet(walletDTO);
         return WalletMapper.toWalletDTO(walletRepository.save(walletToSave));
     }
 
     @Override
+    @Transactional
     public WalletDTO deposit(WalletDTO walletDTO) throws WalletNotFoundException {
         Wallet walletById = getWalletById(walletDTO.getId());
         walletById.setAmount(walletById.getAmount().add(walletDTO.getAmount()));
@@ -46,6 +52,7 @@ public class WalletServiceImpl implements WalletService {
     }
 
     @Override
+    @Transactional
     public WalletDTO withdraw(WalletDTO walletDTO) throws WalletNotFoundException {
         Wallet walletById = getWalletById(walletDTO.getId());
         walletById.setAmount(walletById.getAmount().subtract(walletDTO.getAmount()));
